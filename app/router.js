7717ec7f7e6286c7eba85c86116b6fe4bc9f531f
Mirko Akov <mupkoo@gmail.com>
2014-08-28 15:23:42 +0300

    Create the login template

--- app/router.js
+++ app/router.js
@@ -5,6 +5,8 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+    this.route('login');
+
     this.resource('todos', { path: '/' }, function () {
         this.route('active');
         this.route('completed');
