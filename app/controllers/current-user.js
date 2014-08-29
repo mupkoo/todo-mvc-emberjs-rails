6ee714f754c6726506bd1a7cb27751cf44a7a556
Mirko Akov <mupkoo@gmail.com>
2014-08-29 11:42:12 +0300

    Add check if the user has signed in

--- app/controllers/current-user.js
+++ app/controllers/current-user.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
 
-
+    isSignedIn: function () {
+        return this.get('content') !== null;
+    }.property('content')
 
 });
